d1ee84a0b631646cff89479e91d1c2c98acfc8f0
Dmitriy Startsev <startsevd@gmail.com>
2017-10-31 13:39:21 +0300

    slightly refactored

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 (function() {
+
+    var list = document.querySelector(".cube--extended ul");//ugly but should be global to avoid multiple DOM queries
+
     function init() {
         document.querySelector(".cube--extended").addEventListener("click", displayPrompt);
         var cities = ["saint petersburg", "prague", "becici", "antalya", "atlanta"];
@@ -18,7 +21,7 @@
         function complete(value) {
             document.onkeydown = null;
             searchFormContainer.style.display = "none";
-            queryOpenWeatherMap(value, "forecast", document.querySelector(".cube--extended")) ;
+            queryOpenWeatherMap(value, "forecast", document.querySelector(".cube--extended"));
         }
 
         form.onsubmit = function() {
@@ -49,7 +52,7 @@
     const method = "GET";
     const APIKey = "bf03de8120fbabf27fdd083f9d1fddc0";
     const baseAPICall = "http://api.openweathermap.org/data/2.5/";
-    const metricSystem = "units=metric";
+    const metricSystem = "units=metric";    
 
     function queryOpenWeatherMap(cityName, queryType, cube) {
         var request = new XMLHttpRequest();
@@ -58,11 +61,7 @@
         request.onreadystatechange = function() {
             if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
                 var data = JSON.parse(request.responseText);
-                if (queryType === "forecast") {
-                    renderForecast(data, cube);
-                } else {
-                    renderCurrent(data, cube);
-                }
+                render(data, queryType, cube);
             } else if (request.readyState === XMLHttpRequest.DONE) {
                 alert("bad request");
             }
@@ -70,19 +69,53 @@
         request.send();
     }
 
-    function renderCurrent(data, cube) {
-        var iconClass = "owf owf-" + data["weather"][0]["id"] + " owf-5x";
-        var cityNameSlashTemp = data["name"] + " &#47; " + data["main"]["temp"] + " &#8451;";
+function render(data, queryType, cube) {
+    var iconClass = null;
+    var cityNameSlashTemp = null;
+
+    if (queryType === "weather") {
+        iconClass = "owf owf-" + data["weather"][0]["id"] + " owf-5x";
+        cityNameSlashTemp = data["name"] + " &#47; " + (data["main"]["temp"]).toFixed(0) + " &#8451;";
         cube.querySelector(".icon").setAttribute("class", iconClass);
-        cube.querySelector(".cube__info").innerHTML = cityNameSlashTemp;
+    } else {
+        iconClass = "owf owf-" + data["list"][0]["weather"][0]["id"] + " owf-5x";
+        cityNameSlashTemp = data["city"]["name"] + " &#47; " + data["list"][0]["main"]["temp"].toFixed(0) + " &#8451;";
+        cube.querySelector(".icon").setAttribute("class", iconClass);
+        setList(data);
+    }
+    cube.querySelector(".cube__info").innerHTML = cityNameSlashTemp;
+}
+
+    function getDayOfWeek(timeStamp) {
+        const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+        return days[new Date(timeStamp * 1000).getDay()];
     }
 
-    function renderForecast(data, cube) {
-        console.log(data);
-        var iconClass = "owf owf-" + data["list"][0]["weather"][0]["id"] + " owf-5x";
-        var cityNameSlashTemp = data["city"]["name"] + " &#47; " + data["list"][0]["main"]["temp"] + " &#8451;";
-        cube.querySelector(".icon").setAttribute("class", iconClass);
-        cube.querySelector(".cube__info").innerHTML = cityNameSlashTemp;
+    function setList(data) {
+        list.innerHTML = "";
+        var docFragment = document.createDocumentFragment();
+        for (var i = 1; i < data["list"].length; i += 8) {
+            var listItem = document.createElement("li");
+
+            var dayDiv = document.createElement("div");
+            dayDiv.textContent = getDayOfWeek(data["list"][i]["dt"]);
+            listItem.appendChild(dayDiv);
+
+            var iconDiv = document.createElement("div");
+            var icon = document.createElement("i");
+            var iconClass = "icon owf owf-" + data["list"][i]["weather"][0]["id"] + " owf-2x";
+            icon.setAttribute("class", iconClass);
+            iconDiv.appendChild(icon);
+            listItem.appendChild(iconDiv);
+
+            var tempDiv = document.createElement("div");
+            var temp = data["list"][i]["main"]["temp"].toFixed(0) + " &#8451;";
+            tempDiv.innerHTML = temp;
+            listItem.appendChild(tempDiv);
+
+            docFragment.appendChild(listItem);
+        }
+        list.appendChild(docFragment);
     }
 
     init();
